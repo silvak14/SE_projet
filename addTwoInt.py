34742e33d356d8c5b5694cd0fe12bd80dd12f1ae
Laurecia <KezaLaurences@gmail.com>
2020-07-07 14:31:13 +0400

    ajout addTwoInt.py

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,17 +1,20 @@
 import sys
-
-print(sys.argv)
+def add(a,b):
+    return a+b
+if __name__=="__main__":
+    
+     print(sys.argv)
 if(len(sys.argv)==3):
     x = int( sys.argv[1] )
     y = int( sys.argv[2] )
-    print (x+y)
+    print (add(x,y))
 elif(len(sys.argv)==2):
     x=int(sys.argv[1])
     y=(input("veuillez entrer la troisieme valeur:"))
-    print(x+y)
+    print(add(x,y))
 elif (len(sys.argv)==1):
     x=(input("veuillez entrer la deuxieme  valeur:"))
     y=(input("veuillez entrer la troisieme valeur:"))
-    print(x+y)
+    print(add(x,y))
 else:
     print("erreur")
