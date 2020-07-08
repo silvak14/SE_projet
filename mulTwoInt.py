8a9ca48161a1ea23cca6fd673300b27a682b9ba0
silva <silvakasanji38@gmail.com>
2020-07-08 11:41:51 +0400

    Ajout mulTwoInt.py

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -1,19 +1,20 @@
 import sys
-
-print(sys.argv)
-    
+def mul(x,y):
+    return x+y
+if __name__=="__main__":
+    print(sys.argv)    
 if(len(sys.argv)==3):
     x = int(sys.argv[1])
     y = int(sys.argv[2])
-    print(x+y)
+    print(mul(x,y))
 elif(len(sys.argv)==2):
     x = int(sys.argv[1])
     y = (input("Entrez la troisieme valeur svp ? "))
-    print(x+y)
+    print(mul(x,y))
 elif(len(sys.argv)==1):
     x = (input("Entrez la deuxieme la valeur svp ? "))
     y = (input("Entrez la troixieme valeur svp ? "))
-    print(x+y)
+    print(mul(x+y))
 else:
     print("Desoler erreur")
 
