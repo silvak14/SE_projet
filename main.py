b8ff2e1923855a861a022037dd31f16ae2a7eb74
silva <silvakasanji38@gmail.com>
2020-07-09 14:13:43 +0400

    Fichier main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from mulTwoInt import mul
+import sys
+
+
+
+if __name__ == "__main__":
+	reponse= input("Avez vous besoin de multiplier ?(oui/non)")
+	print(reponse)
+	if(reponse == "oui"):
+		if(len(sys.argv)==3):
+			print(sys.argv)
+			x = int(sys.argv[1])
+			y = int(sys.argv[2])
+			print(mul(x,y))
+		elif(len(sys.argv)==2):
+			x = int(sys.argv[1])
+			y = int(input("Entrez la troisieme valeur svp ? "))
+			print(mul(x,y))
+		elif(len(sys.argv)==1):
+			x = int(input("Entrez la deuxieme valeur svp ? "))
+			y = int(input("Entrez la troisieme valeur svp ? "))
+			print(mul(x,y))
+		else:
+			print("Erreur")
+	else:
+		print("Aurevoir")
