fd8ee1465eef099aaa91e6298920087b8fb30107
Laurecia <KezaLaurences@gmail.com>
2020-07-09 14:42:12 +0400

    ajout all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from mulTwoInt import mul
+from addTwoInt import add
 import sys
-
-
 
 if __name__ == "__main__":
 	reponse= input("Avez vous besoin de multiplier ?(oui/non)")
@@ -22,5 +21,23 @@ if __name__ == "__main__":
 			print(mul(x,y))
 		else:
 			print("Erreur")
+
+            if(reponse == "non"):
+                if(len(sys.argv)==3):
+                        print(sys.argv)
+                        x = int(sys.argv[1])
+                        y = int(sys.argv[2])
+                        print(add(x,y))
+                elif(len(sys.argv)==2):
+                        x = int(sys.argv[1])
+                        y = int(input("Entrez la troisieme valeur svp ? "))
+                        print(add(x,y))
+                elif(len(sys.argv)==1):
+                        x = int(input("Entrez la deuxieme valeur svp ? "))
+                        y = int(input("Entrez la troisieme valeur svp ? "))
+                        print(add(x,y))
+                else:
+                        print("Erreur")
+
 	else:
-		print("Aurevoir")
+            		print("Aurevoir")
